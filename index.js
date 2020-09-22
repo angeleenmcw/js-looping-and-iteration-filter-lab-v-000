130b14e4ba3eb310fba4f5c2c79e199b48893ff0
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-09-22 03:51:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function findMatching (drivers, name) {
-  return drivers.filter(function(arrObj){return arrObj.toLowerCase() === 
+  return drivers.filter(function(arrObj){return arrObj.toLowerCase() === name.toLowerCase});
   
